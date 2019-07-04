cc18320ec2bd510764ecbb6c8922eb4e3f39c2ca
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2019-07-04 18:11:48 +0200

    Add project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int temperature = -20;
+
+        System.out.println(isPositive(temperature));
+    }
+
+    private static boolean isPositive(int temperature){
+        return temperature >= 0;
+    }
+}
